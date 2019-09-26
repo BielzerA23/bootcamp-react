921e222b61bfe3c5a603d08e27ce54ad6fbfbe58
Gabriela Delfino <gabriela.delfino@outlook.com.br>
2019-09-25 21:16:16 -0300

    segunda aula

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React,{Component} from "react";
 import Componente2 from "./Componente2";
 
+class MeuH1 extends Component{
+  render() {
+    console.log(this.props)
+    return <h1>{this.props.title}</h1>
+  }
+}
+
+class MeuBtn extends Component{
+  render(){
+    return <button onClick={()=>this.props.change(this.props.name)}>{this.props.name}</button>
+  }
+}
 
 export default class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      actualBtn: ''
+    }
+  }
+  handleChange = (name) =>{
+    this.setState({
+      actualBtn: name
+    })
+  }
+
+
   render(){
+    console.log(this.state);
     return (
       <>
-      <h1>Hello world!!!</h1>
-      <h2>olá mundo</h2>
-      <Componente2 nome="Gabriel"/>
-      <Componente2 idade="18"/>
+      <MeuH1 title="titulo 01" />
+      <MeuBtn change={this.handleChange} name="botão 01" />
+      <MeuBtn change={this.handleChange} name="botão 02" />
+      <MeuBtn change={this.handleChange} name="botão 03" />
       </>
       )
   }
